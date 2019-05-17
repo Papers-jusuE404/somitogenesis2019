874312adc38db5d107aa47d824ac44b3ea4e400d
xibarrasoria <ximena.ibarra@cruk.cam.ac.uk>
2019-05-17 11:28:43 +0100

    add sample names and mapping stats

--- RNA-seq/scripts/dataProcessing/01.3_countMatrix.R
+++ RNA-seq/scripts/dataProcessing/01.3_countMatrix.R
@@ -16,4 +16,16 @@ for(i in 2:length(samples)){
   data <- cbind(data, read.table(paste0(samples[i], "/ReadsPerGene.out.tab"), row.names = 1, stringsAsFactors = FALSE)[,1])
 }
 colnames(data)[-1] <- unlist(lapply(samples, function(x) unlist(strsplit(x, "/"))[11]))
+
+## edit column names to have sample names instead of library id
+meta <- read.table(paste0(dir, "RNA-seq/data/metadata_RNAseq.tsv"), stringsAsFactors = FALSE, header = TRUE)
+data <- data[,c("gene",intersect(colnames(data), meta$library))] ## remove samples from stage39 which are not replicated and will not be used
+colnames(data)[-1] <- meta[match(colnames(data)[-1], meta$library),]$sample
+
+## separate mapping stats
+mapping.stats <- data[1:4,-1]
+data <- data[-c(1:4),]
+
+## save results
 write.table(data, paste0(dir, "RNA-seq/data/geneCounts.RAW.tsv"), quote = FALSE, sep="\t")
+write.table(t(mapping.stats), paste0(dir, "RNA-seq/data/mappingStatistics.tsv"), quote = FALSE, sep="\t")
